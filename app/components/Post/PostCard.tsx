395d3bbb8332997d447afc2808d9749a5421a5c0
poximy <76846708+poximy@users.noreply.github.com>
2022-04-15 13:33:08 -0500

    fix: change border on hover

--- app/components/Post/PostCard.tsx
+++ app/components/Post/PostCard.tsx
@@ -6,7 +6,10 @@ import { Post } from '@prisma/client';
 const PostCard: FC<Post> = ({ id, title, text }) => {
   return (
     <Link href={`/post/${id}`}>
-      <a className='dark-body w-frac border-body cursor-pointer rounded p-2'>
+      <a
+        className='dark-body w-frac border-body cursor-pointer rounded p-2
+        hover:border-black/50 dark:hover:border-white/50'
+      >
         <p className='w-fit text-lg font-bold line-clamp-1'>{title}</p>
         <p className='text-justify line-clamp-6'>{text}</p>
       </a>
